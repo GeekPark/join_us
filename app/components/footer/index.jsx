ee9103d8f7c2ab143a7617628d05436caa8143d8
Cedcn <cedcn@qq.com>
2015-07-18 19:30:14 +0800

    添加footer模块

---
+++ app/components/footer/index.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var pageData = require('pageData');
+require('./index.styl');
+
+var Footer = React.createClass({
+  render: function () {
+    return (
+      <div className="footer-wrap">
+        <div className="footer-logo"><img src={pageData.footer.logo} /></div>
+        <div className="footer-title">{pageData.footer.title}</div>
+        <h3 className="footer-apply">如何申请？</h3>
+        <div className="footer-email">
+          <span>发送简历至</span>
+          <a href={pageData.footer.email}>{pageData.footer.email}</a>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Footer;
--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ var Header = require('./components/header');
 var About = require('./components/about');
 var Culture = require('./components/culture');
 var Modal = require('./components/modal');
+var Footer = require('./components/footer');
 
 require('./main.styl');
 
@@ -18,6 +19,7 @@ var Layout = React.createClass({
           <Culture />
         </section>
         <Modal title="前端开发工程师" show="false" />
+        <Footer/>
       </div>
     );
   }
